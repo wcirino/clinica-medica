0b924a33a732a92b1ba09b93aa26c3799f55f658
Willyan Cirino <wff.cirino@gmail.com>
2021-04-23 18:47:26 -0300

    primeira versão

---
+++ src/test/java/com/clinicamedica/ClinicaMedicaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.clinicamedica;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ClinicaMedicaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
